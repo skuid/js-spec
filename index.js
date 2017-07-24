43edfb0bbf2ba22d54c013fce3b7f2d59e6edd8d
MRHwick <MatthewRHardwick@gmail.com>
2017-07-24 09:34:45 -0400

    Allow users to set options with defaults for the server connection

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 "use strict";
 
-const Server = require("./server");
+const _ = require("lodash");
 
-Server.start((err) => {
-	if(err) {
-		Server.log(["error"], "Failed to start server");
-		throw err;
-	}
-	Server.log(["info"], "Starting server at " + Server.info.uri);
-});
+function runServer(serverOptions) {
+
+	const Server = require("./server");
+
+	server.connection(_.defaults(options, {
+		host: '127.0.0.1',
+		port: 3000
+	}));
+
+	Server.start((err) => {
+		if(err) {
+			Server.log(["error"], "Failed to start server");
+			throw err;
+		}
+		Server.log(["info"], "Starting server at " + Server.info.uri);
+	});
+}
+
+module.exports = runServer;
